0f01b4bb76eba47f285b5b9dd7e4eef3bb4254a3
Erikcm99 <erikcm9999@gmail.com>
2026-09-01 20:18:48 +0200

    fix basepath

--- src/main.py
+++ src/main.py
@@ -4,10 +4,11 @@ import os
 import sys
 def main():
     path = os.path.dirname(__file__)
-    if not sys.argv[0]:
+    if not sys.argv[1]:
         basepath = '/'
     else:
-        basepath = sys.argv[0]
+        basepath = sys.argv[1]
+    print(f"basepath: {basepath}")
     copy_recursive(os.path.join(path,"../static/"),os.path.join(path,"../docs/"))
     generate_page_recursive(os.path.join(path,"../content"),os.path.join(path,"../template.html") ,os.path.join(path,"../docs"), basepath)
     #generate_page(os.path.join(path,"../content/index.md"),os.path.join(path,"../template.html") ,os.path.join(path,"../public/index.html"))
